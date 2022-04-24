1cd1acaa1df4a2873c391db694a1cdd7f73f5040
Rizwan Mushtaq <rizwan.mushtaq@ottonova.de>
2022-04-24 14:54:28 +0200

    sending post and get requets to firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
+
+import { Post } from './post.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'making-Http-Requests';
+export class AppComponent implements OnInit {
+  loadedPosts = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {}
+
+  onCreatePost(postData: { title: string; content: string }) {
+    // Send Http request
+    // this.http
+    //   .post(
+    //     'https://ng-complete-guide-c56d3.firebaseio.com/posts.json',
+    //     postData
+    //   )
+    //   .subscribe(responseData => {
+    //     console.log(responseData);
+    //   });
+    console.log(postData);
+    this.http
+      .post<{ name: string }>(
+        'https://angular-makinghttprequests-default-rtdb.firebaseio.com/posts.json',
+        postData
+      )
+      .subscribe((responseData) => {
+        console.log(responseData);
+      });
+  }
+
+  onFetchPosts() {
+    // Send Http request
+    this.http
+      .get<{ [key: string]: Post }>(
+        'https://angular-makinghttprequests-default-rtdb.firebaseio.com/posts.json'
+      )
+      .pipe(
+        map((responseData) => {
+          const postsArray: Post[] = [];
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              postsArray.push({ ...responseData[key], id: key });
+            }
+          }
+          return postsArray;
+        })
+      )
+      .subscribe((responseData) => {
+        console.log(responseData);
+      });
+  }
+
+  onClearPosts() {
+    // Send Http request
+  }
 }
