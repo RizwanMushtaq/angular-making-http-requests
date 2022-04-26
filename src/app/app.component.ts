9756fda40811228de77dd8f5cade7fa646bde3ee
Rizwan Mushtaq <rizwan.mushtaq@ottonova.de>
2022-04-26 08:00:18 +0200

    testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -38,6 +38,7 @@ export class AppComponent implements OnInit {
   onClearPosts() {
     this.postsService.deletePosts().subscribe(() => {
       this.loadedPosts = [];
+      //testing
     });
   }
 }
